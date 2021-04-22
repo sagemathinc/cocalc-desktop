4ec69671054c0a68c1a912a26331cc396c508345
William Stein <wstein@sagemath.com>
2021-04-21 17:02:41 -0700

    load app; better size; get rid of zooms

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },

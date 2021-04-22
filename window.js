c76f2f3669ffa6c8b748cd30dfcc660a07be556a
William Stein <wstein@sagemath.com>
2021-04-22 09:33:40 -0700

    just use the default window size for now

--- window.js
+++ window.js
@@ -3,8 +3,6 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
